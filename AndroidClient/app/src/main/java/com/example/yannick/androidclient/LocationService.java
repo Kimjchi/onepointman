7e296381200cf5d674d4a5c17f7d9d060ea6015b
Unknown <arnaud.ricaud.34@gmail.com>
2017-12-04 21:31:10 -0500

    Localisation et marqueur

---
+++ AndroidClient/app/src/main/java/com/example/yannick/androidclient/LocationService.java
@@ -0,0 +1,90 @@
+package com.example.yannick.androidclient;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+/**
+ * Created by Arnaud Ricaud on 04/12/2017.
+ */
+
+public class LocationService implements LocationListener{
+
+    //Instance de la classe
+    private static LocationService instance = null;
+
+    private LocationManager locationManager;
+    private Location location;
+
+    //Méthode pour récupérer l'instance de la classe
+    public static LocationService getLocationService(Context context){
+        if (instance == null) {
+            instance = new LocationService(context);
+        }
+        return instance;
+    }
+
+    //Constructeur:
+    private LocationService( Context context )     {
+
+        this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        if ( ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED){
+                Log.v("ERREUR", "Impossible de créer l'instance de localisation: permission refusée");
+                return ;
+        }
+
+        try {
+            // Status des connections Wi-Fi et GPS
+            boolean isGPSEnabled = locationManager.isProviderEnabled("gps");
+
+            if (!isGPSEnabled){
+                //TODO: Pop-up d'avertissement
+                Log.v("ERROR","Service de localisation indisponible!");
+            } else {
+                if (isGPSEnabled)  {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 1, this);
+                    if (locationManager != null)  {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    }
+                }
+            }
+
+
+        } catch (Exception ex)  {
+            Log.v("ERROR", "Error creating location service: " + ex.getMessage() );
+        }
+        Log.v("LOCALISATION", "Instance créée.");
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        location = newLocation;
+        Log.v("POSITION","Longitude: " + newLocation.getLongitude() + "Latitude: " + newLocation.getLatitude());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.v("LOCALISATION", s + " provider activée");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.v("LOCALISATION", s + " provider désactivée");
+    }
+}
--- AndroidClient/app/src/main/java/com/example/yannick/androidclient/map.java
+++ AndroidClient/app/src/main/java/com/example/yannick/androidclient/map.java
@@ -1,18 +1,31 @@
 package com.example.yannick.androidclient;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+import static com.example.yannick.androidclient.LocationService.getLocationService;
+
 public class Map extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationService locationService;
+    private final int REQUEST_PERMISSION_PHONE_LOCATION = 1;
+    private boolean canDisplayMarker = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,51 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Log.v("LOCATION", "Permission précedement refusée");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
+        } else {
+            Log.v("LOCATION", "Demande de permission");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String permissions[],
+            int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_PHONE_LOCATION:
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationService = getLocationService(getApplicationContext());
+                    Log.v("PERMISSION", "ACCEPT!");
+                    if (locationService != null) {
+                        Log.v("LOCALISATION", "Condition remplie");
+                        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationService);
+                        canDisplayMarker = true;
+                        Log.v("LOCALISATION", "J'affiche ma position!");
+                        displayMyPosition();
+                    } else {
+                        Log.v("LOCALISATION", "Condition NON remplie");
+                    }
+                } else {
+                    Log.v("PERMISSION", "DENIED!");
+                }
+        }
+    }
+
+    public boolean isLocationPermissionGranted() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.v("PERMISSION", "ACCEPT!");
+            return true;
+        } else {
+            Log.v("PERMISSION", "DENIED!");
+            return false;
+        }
+
     }
 
 
@@ -37,10 +95,17 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    public void displayMyPosition() {
+        if (canDisplayMarker && (mMap != null)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Location newLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(newLocation.getLatitude(), newLocation.getLongitude()))
+                        .title("Hello world"));
+            }
+        }
     }
 }

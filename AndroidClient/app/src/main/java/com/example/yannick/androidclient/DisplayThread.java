f05c6836970be19a4e15fa63771735d223fc4e83
Unknown <arnaud.ricaud.34@gmail.com>
2017-12-05 19:16:53 -0500

    [Android] Location: Correction 'nullPointerException'

---
+++ AndroidClient/app/src/main/java/com/example/yannick/androidclient/DisplayThread.java
@@ -0,0 +1,65 @@
+package com.example.yannick.androidclient;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by Arnaud Ricaud on 05/12/2017.
+ */
+
+public class DisplayThread implements Runnable {
+    private final int MY_POSITION_UPDATE_TIME = 10000;
+    private Handler handler = new Handler();
+    private boolean displayThreadRunning = true;
+    private Location myLocation;
+
+    private Map activity;
+
+    @Override
+    public void run() {
+        if (displayThreadRunning) {
+            activity = Map.instance;
+            LocationService locationService = LocationService.getLocationService(activity.getActivity().getApplicationContext());
+            LocationManager locationManager;
+            locationManager = (LocationManager) activity.getActivity().getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(activity.getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Log.v("Location", "Updating position...");
+                myLocation = locationService.getLocation();
+                if (myLocation != null) {
+                    activity.getActivity().runOnUiThread(new Thread(new Runnable() {
+                        public void run(){
+                        if ((myLocation != null) && (activity.mMap != null)) {
+                            activity.mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()))
+                                    .title("Here I am!"));
+                            Toast.makeText(activity.getActivity().getApplicationContext(), "Longitude(" + myLocation.getLongitude() + ")\nLatitude(" + myLocation.getLatitude()+")", Toast.LENGTH_LONG).show();
+                        }
+                        Log.v("LOCATION", "Update displayed!");
+                        Log.v("POSITION", "Longitude: " + myLocation.getLongitude() + " Latitude: " + myLocation.getLatitude());
+
+                        }
+                    }));
+                }
+                handler.postDelayed(this, MY_POSITION_UPDATE_TIME);
+            }
+        }
+    }
+
+    public void stopDisplay(){
+        displayThreadRunning = false;
+    }
+    public boolean getdisplayThreadRunning(){
+        return displayThreadRunning;
+    }
+}
--- AndroidClient/app/src/main/java/com/example/yannick/androidclient/Map.java
+++ AndroidClient/app/src/main/java/com/example/yannick/androidclient/Map.java
@@ -4,11 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -29,26 +27,14 @@ import static com.example.yannick.androidclient.LocationService.getLocationServi
 
 public class Map extends Fragment implements OnMapReadyCallback {
     //call this method in your onCreateMethod
-    private GoogleMap mMap;
+    public GoogleMap mMap;
     private final int REQUEST_PERMISSION_PHONE_LOCATION = 1;
-    private final int MY_POSITION_UPDATE_TIME = 10000;
+
     private LocationManager locationManager;
     private LocationService locationService;
-    private Location myLocation;
-    private Handler handler = new Handler();
-
-    private Thread displayMyPosition = new Thread(new Runnable() {
-        public void run() {
-            if ((myLocation != null) && (mMap != null)) {
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()))
-                        .title("Here I am!"));
-            }
-            Toast.makeText(getActivity().getApplicationContext(), "Longitude(" + myLocation.getLongitude() + ")\nLatitude(" + myLocation.getLatitude()+")", Toast.LENGTH_LONG).show();
-        }
-    });
-
-    private Thread updateMyPosition;
+    private DisplayThread updateMyPosition;
+    public static Map instance = null;
+    Location myLocation;
 
     @Nullable
     @Override
@@ -59,23 +45,14 @@ public class Map extends Fragment implements OnMapReadyCallback {
     @Override
     public void onStart(){
         super.onStart();
-        //On regarde si on a la permission d'utiliser la localisation de l'utilisateur et on lui demande si ce n'est pas le cas.
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
-            Log.v("LOCATION", "Permission précedement refusée");
-        } else {
-            Log.v("LOCATION", "Demande de permission");
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
-        }
+        instance = this;
         startGpsService();
     }
-
 
     @Override
     public void onStop(){
         super.onStop();
-        updateMyPosition.interrupt();
-        Log.v("GPS Service", "Display position STOPPED");
+        stopDisplayThread();
     }
 
     @Override
@@ -92,46 +69,64 @@ public class Map extends Fragment implements OnMapReadyCallback {
     }
 
     public void startGpsService() {
-        //On start le service gps si l'instance n'est pas créée et qu'on a les droits:
-        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            Log.v("Location", "Permission granted!");
-            startLocationService();
-        } else {
-            Log.v("Location", "Permission denied!");
+        //On demande les droits à l'utilisateur:
+        askLocationPermission();
+        //si on a les droits:
+        if (checkPermission()){
+            boolean serviceStarted = startLocationService();
+            //Si le service est lancé
+            if (serviceStarted){
+                //On lance le thread lié au display
+                startDisplayThread();
+            }
         }
     }
 
     @SuppressLint("MissingPermission") //Permission déjà vérifiée!
-    public void startLocationService() {
+    public boolean startLocationService() {
         Log.v("GPS Service", "Starting service...");
         locationService = getLocationService(getActivity().getApplicationContext());
         if (locationService != null) {
             locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, locationService);
             Log.v("GPS Service", "Service STARTED!");
-            updateMyPosition = newDisplayUpdateThread();
-            updateMyPosition.start();
-            Log.v("GPS Service", "Display position every " + MY_POSITION_UPDATE_TIME + "ms");
+            return true;
         } else {
             Log.v("GPS Service", "ERROR: Impossible to get instance...");
+            return false;
         }
     }
 
-    public Thread newDisplayUpdateThread(){
-        return new Thread(new Runnable() {
-            private Handler handler = new Handler();
-
-            public void run() {
-                Log.v("Location", "Updating position...");
-                myLocation = locationService.getLocation();
-                getActivity().runOnUiThread(displayMyPosition);
-                Log.v("Location", "Display update!");
-                Log.v("POSITION","Longitude: " + myLocation.getLongitude() + " Latitude: " + myLocation.getLatitude());
-                handler.postDelayed(this, MY_POSITION_UPDATE_TIME);
-            }
-        });
+    public void askLocationPermission(){
+        //On regarde si on a la permission d'utiliser la localisation de l'utilisateur et on lui demande si ce n'est pas le cas.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
+            Log.v("LOCATION", "Permission précedement refusée");
+        } else {
+            Log.v("LOCATION", "Demande de permission");
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_PHONE_LOCATION);
+        }
     }
 
+    public boolean checkPermission(){
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.v("Location", "Permission granted!");
+            return true;
+        } else {
+            Log.v("Location", "Permission denied!");
+            return false;
+        }
+    }
+    public void startDisplayThread() {
+        updateMyPosition = new DisplayThread();
+        updateMyPosition.run();
+        Log.v("GPS Service", "Display thread started");
+    }
 
+    public void stopDisplayThread() {
+        if (updateMyPosition.getdisplayThreadRunning()) {
+            updateMyPosition.stopDisplay();
+            Log.v("GPS Service", "Display thread stopped");
+        }
+    }
 }

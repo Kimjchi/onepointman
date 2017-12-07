917ff22c26debb0bbaf7295aa302da50b49b1947
kimjii <kimj77600@gmail.com>
2017-12-06 20:30:13 -0500

    [React] Préparations des requêtes pour les groupes et utilisateurs

--- client/src/containers/GroupsContainer.js
+++ client/src/containers/GroupsContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {addGroup, changeNomGroupe} from "../actions/opGroups";
+import {addGroup, changeGroupName} from "../actions/opGroups";
 import GroupsComponent from "../components/GroupsComponent";
 import {addUser} from "../actions/opUsers";
 
@@ -9,13 +9,13 @@ class GroupsContainer extends Component {
 
 
     render() {
-        let {groupes, nomGroupe} = this.props.opGroups;
-        let {groupesUsers} = this.props.opUsers;
+        let {groups, groupName} = this.props.opGroups;
+        let {groupsUsers} = this.props.opUsers;
         return (
-            <GroupsComponent groupes={groupes}
-                             nomGroupe={nomGroupe}
-                             groupesUsers={groupesUsers}
-                             changeNomGroupe={this.props.changeNomGroupe}
+            <GroupsComponent groups={groups}
+                             groupName={groupName}
+                             groupsUsers={groupsUsers}
+                             changeGroupName={this.props.changeGroupName}
                              addGroup={this.props.addGroup}
                              addUser={this.props.addUser}
             />
@@ -34,8 +34,8 @@ function mapStateToProps (state) {
 //fonctions
 const  mapDispatchToProps = (dispatch) => {
     return{
-        changeNomGroupe: (nomGroupe) => {
-            dispatch(changeNomGroupe(nomGroupe));
+        changeGroupName: (groupName) => {
+            dispatch(changeGroupName(groupName));
         },
         addGroup: (arrayGroups) => {
             dispatch(addGroup(arrayGroups));
--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -1,8 +1,10 @@
 import {all} from 'redux-saga/effects';
 import {LoginFlow} from "./LoginSaga";
 import {MapFlow} from "./MapSaga";
+import {GroupsFlow} from "./GroupsSaga";
+import {UsersFlow} from "./UsersSaga";
 
 export default function * root(){
 
-    yield all ([LoginFlow(), MapFlow()])
+    yield all ([LoginFlow(), MapFlow(), GroupsFlow(), UsersFlow()])
 }

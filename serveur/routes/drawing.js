e77e93a5745ebfb1878ba652a2d4b3a35ca33bff
Clara Garbe Ortega <clara.g-o@hotmail.fr>
2017-12-09 00:02:05 -0500

    create drawing non teste parce que po ltemps. reste "desactiver dessin" (ez shit)

--- serveur/routes/drawing.js
+++ serveur/routes/drawing.js
@@ -9,4 +9,37 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post('/createdrawing', function(req,res){
+    let toCreate = {
+        idcreator: req.body.iduser,
+        idgroup: req.body.idgroup,
+        description: req.body.description,
+        lt: req.body.lt,
+        lg: req.body.lg,
+        img: req.body.img
+    };
+
+    let query = squel.insert()
+        .into('public."DRAWING"')
+        .set('idcreator', toCreate.idcreator)
+        .set('idgroup', toCreate.idgroup)
+        .set('description', toCreate.description)
+        .set('drawinglt', toCreate.lt)
+        .set('drawinglg', toCreate.lg)
+        .set('img', toCreate.img)
+        .toString();
+
+    db.one(query)
+        .then((row)=>{
+            let response = {
+                iddrawing : row.iddrawing
+            }
+            sender.sendResponse(sender.SUCCESS_STATUS, response, res)
+        })
+        .catch(e => {
+            sender.sendResponse(sender.NOT_FOUND_STATUS, 'Error while creating drawing', res);
+            console.log(e);
+        })
+});
+
 module.exports = router;
